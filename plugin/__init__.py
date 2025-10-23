cac8ec7761e884dbd80a40783a6d965aee98554a
Sidney Schaberle Goveia <sidneysgoveia@gmail.com>
2025-10-23 19:16:23 -0300

    dl

--- plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Enhanced Trend Surface Analysis Plugin for QGIS
-"""
-
-def classFactory(iface):
-    from .enhanced_trend_surface import EnhancedTrendSurfacePlugin
-    return EnhancedTrendSurfacePlugin(iface)
